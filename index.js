5a2006f050e0ed397c4ae35980414f6624408bd7
ZelikSV <zeliksv@gamil.com>
2019-01-17 14:53:48 +0200

    some change

--- index.js
+++ index.js
@@ -1,36 +1,31 @@
-function sum() {
-  console.log(1);
-  return [].reduce.call(arguments, (acc, el) => acc+=el);
+function sum(a, b) {
+return a + b;
 }
+const val = 33;
 
-const prom = x => new Promise(res => {
-  console.log(2);
-  setTimeout(res,2000,x);
-})
+const val2 = {name: 'ivan'};
 
-function pow() {
-  console.log(3);
-  return [].reduce.call(arguments, (acc, el) => acc*=el);
-}
+const p = new Promise((res, rej) => {
+  setTimeout(() => {
+   res(1);
+  }, 10);
+});
 
-const arr = [1,2,3,4]
 
 function *gen() {
-  const a = yield sum.bind(null, ...arr);
-  const b = yield prom(a);
-  const c = yield pow.bind(null, ...arr);
-  const d = yield arr;
-  console.log(a + b + c + d)
-  yield a + b + c + d;
-}
+  const a = yield sum(1, 2);
+  const b = yield p;
+  const c = yield val2;
+  const d = yield val;
 
-function checker(value, resArray){
-
+  console.log(a + b + c + d);
 }
+const iterator = gen();
 
 async function runnerFunction(arg){
- 
-}
+ const resArray = [];
 
+}
+runnerFunction(iterator);
 
 // runnerFunction(gen()).then(data => console.log(data.pop() === '441,2,3,4' ? "Good Job" : "You are fail this task"));
