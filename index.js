10a346f7d85034dc5ad75d794ea03dc1848f832c
ZelikSV <zeliksv@gamil.com>
2019-01-17 16:31:40 +0200

    implementation change

--- index.js
+++ index.js
@@ -1,60 +1,60 @@
 function sum() {
   console.log(1);
-  return [].reduce.call(arguments, (acc, el) => acc+=el);
+  return [].reduce.call(arguments, (acc, el) => (acc += el));
 }
 
-const prom = x => new Promise(res => {
-  console.log(2);
-  setTimeout(res,2000,x);
-})
+const prom = x =>
+  new Promise(res => {
+    console.log(2);
+    setTimeout(res, 2000, x);
+  });
 
 function pow() {
   console.log(3);
-  return [].reduce.call(arguments, (acc, el) => acc*=el);
+  return [].reduce.call(arguments, (acc, el) => (acc *= el));
 }
 
-const arr = [1,2,3,4]
+const arr = [1, 2, 3, 4];
 
-function *gen() {
+function* gen() {
   const a = yield sum.bind(null, ...arr);
   const b = yield prom(a);
   const c = yield pow.bind(null, ...arr);
   const d = yield arr;
-  console.log(a + b + c + d)
+  console.log(a + b + c + d);
   yield a + b + c + d;
 }
-function checker(value, resArray){
-  if(typeof value === 'function'){
-    resArray.push(value);
-    return value();
-    }else if(value === undefined){
-      return;
-    }else if(value instanceof Promise){
-       value.then(data => {
-        resArray.push(data);
-        return value;
-       });
-    }else{
-      resArray.push(value);
-      return value;
-    }
-}
 
-async function runnerFunction(arg){
+function runnerFunction(arg) {
   const resArray = [];
-  let nextIteretion = arg.next();
-  let flag = nextIteretion.done;
-  let value = await nextIteretion.value;
-  let res = checker(value, resArray);
-  while(flag === false){
-    await value;
-    nextIteretion = arg.next(res);
-    flag = nextIteretion.done;
-    value = await nextIteretion.value;
-    res = checker(value, resArray);
-  }
-  return resArray;
+
+  return new Promise(resolve => {
+    function executer(iterator, prevValue) {
+      const nextIteretion = iterator.next(prevValue);
+      let { value, done } = nextIteretion;
+
+      if (!done) {
+        if (typeof value === "function") {
+          resArray.push(value());
+          return executer(iterator, value());
+        } else if (value instanceof Promise) {
+          return value.then(res => {
+            resArray.push(res);
+            executer(iterator, res);
+          });
+        } else {
+          resArray.push(value);
+          return executer(iterator, value);
+        }
+      }
+      return resolve(resArray);
+    }
+    executer(arg);
+  });
 }
 
-
-runnerFunction(gen()).then(data => console.log(data.pop() === '441,2,3,4' ? "Good Job" : "You are fail this task"));
+runnerFunction(gen()).then(data =>
+  console.log(
+    data.pop() === "441,2,3,4" ? "Good Job" : "You are fail this task"
+  )
+);
