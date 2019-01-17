e82e20446f825745e346f662c29824936e38ac53
ZelikSV <zeliksv@gamil.com>
2019-01-17 13:33:04 +0200

    start task

--- index.js
+++ index.js
@@ -23,38 +23,14 @@ function *gen() {
   console.log(a + b + c + d)
   yield a + b + c + d;
 }
+
 function checker(value, resArray){
-  if(typeof value === 'function'){
-    resArray.push(value);
-    return value();
-    }else if(value === undefined){
-      return;
-    }else if(value instanceof Promise){
-       value.then(data => {
-        resArray.push(data);
-        return value;
-       });
-    }else{
-      resArray.push(value);
-      return value;
-    }
+
 }
 
 async function runnerFunction(arg){
-  const resArray = [];
-  let nextIteretion = arg.next();
-  let flag = nextIteretion.done;
-  let value = await nextIteretion.value;
-  let res = checker(value, resArray);
-  while(flag === false){
-    await value;
-    nextIteretion = arg.next(res);
-    flag = nextIteretion.done;
-    value = await nextIteretion.value;
-    res = checker(value, resArray);
-  }
-  return resArray;
+ 
 }
 
 
-runnerFunction(gen()).then(data => console.log(data.pop() === '441,2,3,4' ? "Good Job" : "You are fail this task"));
+// runnerFunction(gen()).then(data => console.log(data.pop() === '441,2,3,4' ? "Good Job" : "You are fail this task"));
